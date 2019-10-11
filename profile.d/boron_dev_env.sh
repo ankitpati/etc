ae2eec535c628a9829fa29b7abd70aa15ebf6321
Ankit Pati <contact@ankitpati.in>
2019-10-11 09:11:11 +0530

    global shell environment

---
+++ profile.d/boron_dev_env.sh
@@ -0,0 +1,43 @@
+function sanitize_path
+{
+    # Utility function to sanitize PATH-like specifications.
+    # Do not allow repeated elements, repeated, starting, or ending `:`.
+    echo -n "$1" | awk -v 'RS=:' -v 'ORS=:' '!seen[$0]++' \
+                 | sed 's/::*/:/g' | sed 's/^://' | sed 's/:$//'
+}
+
+export PERL_CPANM_OPT='--mirror https://cpan.metacpan.org/'
+export PERLBREW_CPAN_MIRROR='https://cpan.metacpan.org/'
+export PYENV_ROOT="$HOME/.pyenv"
+export POWERSHELL_TELEMETRY_OPTOUT='1'
+
+export HISTSIZE=''
+export HISTCONTROL='ignorespace:ignoredups'
+
+alias l='ls -CF'
+alias la='ls -A'
+alias tree='tree -I .git'
+alias podchecker='podchecker -warnings -warnings -warnings'
+alias bat='bat --paging=never --style=plain --wrap=never --'
+alias tohex="hexdump -ve '1/1 \"%.2x\" '"
+alias unchomp='sed -i -e \$a\\ '
+alias ssh='exec ssh'
+alias telnet='exec telnet'
+alias mosh='exec mosh'
+alias git-sh='exec git-sh'
+alias ssh-copy-id='ssh-copy-id -oPasswordAuthentication=yes'
+
+function _git_pick
+{
+    _git_branch
+}
+
+function _git_publish
+{
+    _git_branch
+}
+
+function _git_files
+{
+    _git_branch
+}
