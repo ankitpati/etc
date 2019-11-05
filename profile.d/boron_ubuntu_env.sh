b67dc41e5a27aee7e5fb5503fc55c9c64341d229
Ankit Pati <contact@ankitpati.in>
2019-11-05 10:31:12 +0530

    less fancy prompt

--- profile.d/boron_ubuntu_env.sh
+++ profile.d/boron_ubuntu_env.sh
@@ -5,7 +5,7 @@ shopt -s histappend
 eval "$(SHELL='/bin/sh' lesspipe)"
 
 # Ubuntu-specific colourful prompt
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+PS1='${debian_chroot:+($debian_chroot)}[\u@\h \W]\$ '
 PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
 
 eval "$(dircolors -b)"
