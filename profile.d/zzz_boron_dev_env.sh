182135680bd79eea1ef2706ac7afdf1655604813
Ankit Pati <contact@ankitpati.in>
2020-05-19 23:17:53 +0530

    zzz_boron_dev_env.sh: update to current usage

--- profile.d/zzz_boron_dev_env.sh
+++ profile.d/zzz_boron_dev_env.sh
@@ -3,14 +3,18 @@ mesg n || true
 function sanitize_path
 {
     # Utility function to sanitize PATH-like specifications.
-    # Do not allow repeated elements, repeated, starting, or ending `:`.
+    # Do not allow
+    # 1. repeated elements,
+    # 2. repeated, starting, or ending `:`, and
+    # 3. repeated `/`.
     echo -n "$1" | awk -v 'RS=:' -v 'ORS=:' '!seen[$0]++' \
-                 | sed 's/::*/:/g' | sed 's/^://' | sed 's/:$//'
+                 | sed 's/::*/:/g' | sed 's/^://' | sed 's/:$//' \
+                 | sed 's_//*_/_g'
 }
 
 export PERL_CPANM_OPT='--mirror https://cpan.metacpan.org/'
 export PERLBREW_CPAN_MIRROR='https://cpan.metacpan.org/'
-export PERL5LIB="$(sanitize_path "$HOME/lib/perl5/")"
+export PERL5LIB="$HOME/lib/perl5/"
 export PYENV_ROOT="$HOME/.pyenv/"
 export NPM_PACKAGES="$HOME/.npm/packages/"
 export POWERSHELL_TELEMETRY_OPTOUT='1'
@@ -24,13 +28,9 @@ export PROMPT_COMMAND="history -a; history -n; $PROMPT_COMMAND"
 export EDITOR='vim'
 export MERGE='vimdiff'
 
-alias ls='ls --color=auto'
-alias l='ls -CF'
-alias la='ls -A'
-alias ll='ls -alF --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
+manpath="$MANPATH"
+unset MANPATH
+export MANPATH="$(sanitize_path "$manpath:$(manpath)")"
 
 alias tree='tree -I ".git|node_modules"'
 alias cpan-outdated='cpan-outdated --mirror="$PERLBREW_CPAN_MIRROR"'
