f5d4a16591c6407ce0e89cdf7ec2c820a2b97c96
Ankit Pati <contact@ankitpati.in>
2021-03-21 20:32:10 +0530

    toolbox-Effect: Shift All That Can Stay Home
    Fedora `toolbox` reads `~`, but not `/etc/`, causing serious
    side-effects. I may or may not have lost all my accumulated bash history
    to such a side-effect.
    
    This commit gets rid of all config that can be placed in `~` as
    dotfiles. Refer to ankitpati/dotfiles to know the fate of these files.

--- profile.d/zzz_boron_ubuntu_env.sh
+++
@@ -1,8 +0,0 @@
-# Debian-specific wrappers for `less`
-eval "$(SHELL='/bin/sh' lesspipe)"
-
-# Ubuntu-specific colourful prompt
-PS1='${debian_chroot:+($debian_chroot)}[\u@\h \W]\$ '
-PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-
-eval "$(dircolors -b)"
